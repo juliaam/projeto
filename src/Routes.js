765bcb26803b4377b82369c014ee7b7d3dded1ba
juliaam <juliademoraess@gmail.com>
2024-06-17 14:23:49 -0300

    fix: eslint problems

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,13 @@
-import { Route, Routes } from "react-router-dom";
-import { CarPage } from "./pages/Car/index.tsx";
-import { CarFormPage } from './pages/Car/formulario/index.tsx'
-
+import { Route, Routes } from 'react-router-dom';
+import { CarPage } from './pages/Car/index.tsx';
+import { CarFormPage } from './pages/Car/formulario/index.tsx';
+import React from 'react';
 
 export function AppRouter() {
-    return (
-        <Routes>
-            <Route path="/" element={<CarPage />} />
-            <Route path="/carro/formulario" element={<CarFormPage />} />
-        </Routes>)
+  return (
+    <Routes>
+      <Route path="/" element={<CarPage />} />
+      <Route path="/carro/formulario" element={<CarFormPage />} />
+    </Routes>
+  );
 }
